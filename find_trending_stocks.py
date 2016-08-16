fcc4b0ec8473027d909b70a73b990bdac13986d0
Tyree Stevenson <tyreeostevenson@gmail.com>
2016-08-16 00:26:15 -0400

    fixed file names

--- find_trending_stocks.py
+++ find_trending_stocks.py
@@ -7,7 +7,7 @@ def find_trending_stocks():
     stock_news_website_scrapper = BeautifulSoup(stock_news_website, 'html.parser') # parses sites html
     stock_website_text = stock_news_website_scrapper.findAll(text=True) # grabs text from parse html
     for words in stock_website_text:
-        if  3 <= len(words) <= 4 and str(words).isupper() == True:
+        if  3 <= len(words) <= 5 and str(words).isupper() == True:
             print(words) # prints out words that are stock names
 
 
