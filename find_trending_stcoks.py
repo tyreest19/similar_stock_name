35933990e28b82cb433a8f28ef4d117ae3808e07
Tyree Stevenson <tyreeostevenson@gmail.com>
2016-08-16 00:25:15 -0400

    initial commit

---
+++ find_trending_stcoks.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def find_trending_stocks():
+    """Finds trending stock names from marketwatch.com/newsviewer"""
+    stock_news_website = urllib.request.urlopen('http://www.marketwatch.com/newsviewer').read() # gets httml from the site
+    stock_news_website_scrapper = BeautifulSoup(stock_news_website, 'html.parser') # parses sites html
+    stock_website_text = stock_news_website_scrapper.findAll(text=True) # grabs text from parse html
+    for words in stock_website_text:
+        if  3 <= len(words) <= 4 and str(words).isupper() == True:
+            print(words) # prints out words that are stock names
+
+
+
+
+if __name__ == '__main__':
+    find_trending_stocks()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ similar_stock_name.py
@@ -0,0 +1,38 @@
+from difflib import SequenceMatcher
+
+def similar_stock_name_checker(stock_name, similar_stock_name):
+    """Takes a stockname and detrimines if similar name is similar if its above 70% similar it appends
+    to a file."""
+    if SequenceMatcher(None, stock_name, similar_stock_name).ratio() >= 0.70:
+       file_with_stored_names = open('similar_stock_names.txt', 'a')
+       file_with_stored_names.write('{} similar names are {}'.format(stock_name,similar_stock_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    stock_name = input('stock name: ')
+    suggest_name = input('similar name: ')
+    print(similar_stock_name_checker(stock_name, suggest_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
